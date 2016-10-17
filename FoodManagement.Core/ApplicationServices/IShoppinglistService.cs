c8d9fc275739b3e850dc789fde0ea4f44e6c02b6
Jens <jvandendriessche@mmm.com>
2016-10-17 13:24:50 +0200

    Add ShoppinglistService class and interface to applicationServices in Core library with empty functions. Add IGenericRepository and IUnitOfWork Interfaces to Infrastructre in Core library

---
+++ FoodManagement.Core/ApplicationServices/IShoppinglistService.cs
@@ -0,0 +1,14 @@
+﻿using FoodManagement.Core.Model;
+using System;
+using System.Collections.Generic;
+
+namespace FoodManagement.Core
+{
+    interface IShoppinglistService
+    {
+        void AddItemToFamilyShoppinglist(Guid PersonId, ShoppinglistItem item);
+        IEnumerable<ShoppinglistItem> GetFamilyShoppinglist(Guid PersonId);
+        void MarkShoppinglistItemAsBought(Guid Item);
+        void MarkAllShoppinglistItemsAsBought(Guid FamilyId);
+    }
+}
