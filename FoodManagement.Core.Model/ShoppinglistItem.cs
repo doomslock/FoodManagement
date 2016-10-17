5e8fb87307dff013569511b2c53bdbb27ba83359
Jens <jvandendriessche@mmm.com>
2016-10-17 13:23:30 +0200

    Add Family, Person and ShoppinglistItem classes and properties to Core.Model library

---
+++ FoodManagement.Core.Model/ShoppinglistItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FoodManagement.Core.Model
+{
+    public class ShoppinglistItem
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Store { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
