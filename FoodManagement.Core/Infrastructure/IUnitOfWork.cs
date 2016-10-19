623a4f91974a35a4a438433bc3ae406788267371
Jens <jvandendriessche@mmm.com>
2016-10-19 09:29:05 +0200

    Comment in IUnitOfWork and little changes in diagram

--- FoodManagement.Core/Infrastructure/IUnitOfWork.cs
+++ FoodManagement.Core/Infrastructure/IUnitOfWork.cs
@@ -5,7 +5,7 @@ namespace FoodManagement.Core
     public interface IUnitOfWork
     {
         void Save();
-        IGenericRepository<TEntity> Repository<TEntity>();
+        IGenericRepository<TEntity> Repository<TEntity>();//Implementation user service locator or abstract factory
         void BeginTransaction(IsolationLevel isolationLevel = IsolationLevel.Unspecified);
         void Commit();
         void RollBack();
