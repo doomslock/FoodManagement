13bfa96f57ad9eb5b378d6f1d2bfd9e44bec6229
Jens <jvandendriessche@mmm.com>
2016-10-24 08:46:29 +0200

    Added Test project

---
+++ FoodManagement.Test/ShoppingListServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace FoodManagement.Test
+{
+    [TestClass]
+    public class ShoppinglistServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
