e3063a5a90966660674d1028f4df12ef0ef4441c
Jens <jvandendriessche@mmm.com>
2016-10-27 22:47:59 +0200

    New genericrepositorytest class added to test project

--- FoodManagement.DependencyResolution/DependencyConfiguration.cs
+++ FoodManagement.DependencyResolution/DependencyConfiguration.cs
@@ -11,9 +11,9 @@ namespace FoodManagement.DependencyResolution
         public DependencyConfiguration()
         {
             kernel = new StandardKernel();
-            kernel.Bind<IGenericRepository<Core.Model.Family>>().To<GenericRepository<Core.Model.Family>>();
-            kernel.Bind<IGenericRepository<Core.Model.Person>>().To<GenericRepository<Core.Model.Person>>();
-            kernel.Bind<IGenericRepository<Core.Model.ShoppinglistItem>>().To<GenericRepository<Core.Model.ShoppinglistItem>>();
+            kernel.Bind<IGenericRepository<Family>>().To<GenericRepository<Family>>();
+            kernel.Bind<IGenericRepository<Person>>().To<GenericRepository<Person>>();
+            kernel.Bind<IGenericRepository<ShoppinglistItem>>().To<GenericRepository<ShoppinglistItem>>();
             kernel.Bind<IRepositoryFactory>().To<RepositoryFactory>();
             kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
             kernel.Bind<IShoppinglistService>().To<ShoppinglistService>();
---
+++ FoodManagement.Test/Infrastructure/GenericRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using FoodManagement.Infrastructure.Dal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace FoodManagement.Test.Infrastructure
+{
+    [TestClass]
+    public class GenericRepositoryTest
+    {
+        private GenericRepository<ShoppinglistItem> _rep;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var context = new Mock<FMDbContext>();
+            var dbSet = new Mock<DbSet<ShoppinglistItem>>();
+            List<ShoppinglistItem> sliList = new List<ShoppinglistItem>();
+            dbSet.Setup(d => d.Add(It.IsAny<ShoppinglistItem>())).Callback((ShoppinglistItem sli) => sliList.Add(sli));
+            context.Setup(c => c.Set<ShoppinglistItem>()).Returns(dbSet.Object);
+            _rep = new GenericRepository<ShoppinglistItem>(context.Object);
+        }
+
+        [TestMethod]
+        public void Insert()
+        {
+            
+        }
+    }
+}
