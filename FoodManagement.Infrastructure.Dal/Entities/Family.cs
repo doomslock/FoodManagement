25f191eec240d02abb4bac1129479de4eb73bf1a
Jens <jvandendriessche@mmm.com>
2016-11-09 11:44:47 +0100

    Include FamilyMembers and Shoppinglist properties for Family DataEntity

--- FoodManagement.Infrastructure.Dal/Entities/Family.cs
+++ FoodManagement.Infrastructure.Dal/Entities/Family.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using FoodManagement.Core;
+using System.Collections.Generic;
 
 namespace FoodManagement.Infrastructure.Dal
 {
@@ -11,5 +12,7 @@ namespace FoodManagement.Infrastructure.Dal
         [Key]
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Person> FamilyMembers { get; set; }
+        public ICollection<ShoppinglistItem> Shoppinglist { get; set; }
     }
 }
