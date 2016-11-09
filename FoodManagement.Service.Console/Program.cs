15129208ec999e6bb6d8c6fb04a69119cdb48773
Jens <jvandendriessche@mmm.com>
2016-11-09 14:46:01 +0100

    Add Service.Console project to manually test integration of system.

---
+++ FoodManagement.Service.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using FoodManagement.Core;
+using FoodManagement.DependencyResolution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodManagement.Service.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShoppinglistService shoplistService = new DependencyConfiguration().GetInstance<IShoppinglistService>() as ShoppinglistService;
+
+            var shoppinglist = shoplistService.GetFamilyShoppinglist(Guid.Parse("D38A4709-4D0A-434B-905B-1ADACB7B015E"));
+            foreach (var s in shoppinglist)
+            {
+                System.Console.WriteLine(s.Name + " - " + s.Amount);
+            }
+            System.Console.ReadKey();
+        }
+    }
+}
