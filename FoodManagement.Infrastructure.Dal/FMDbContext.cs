64407b3fcec63beddaf183769e9063b1f03504e1
Jens <jvandendriessche@mmm.com>
2016-11-09 11:46:48 +0100

    Added missing DbSets in FMDbContext

--- FoodManagement.Infrastructure.Dal/FMDbContext.cs
+++ FoodManagement.Infrastructure.Dal/FMDbContext.cs
@@ -8,8 +8,14 @@ namespace FoodManagement.Infrastructure.Dal
     {
         public FMDbContext() : base(@"Data Source=(localdb)\v11.0;Initial Catalog=FoodManagement;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
         {
-            Configuration.LazyLoadingEnabled = false;
+            Configuration.LazyLoadingEnabled = true;
             Configuration.ProxyCreationEnabled = false;
         }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Family> Families { get; set; }
+        public DbSet<ShoppinglistItem> ShoppinglistItems { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Store> Stores { get; set; }
     }
 }
