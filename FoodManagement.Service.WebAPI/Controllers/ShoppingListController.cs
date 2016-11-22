358a08cda9c65f97ddc31d3660df5e8044274e69
Jens <jvandendriessche@mmm.com>
2016-11-22 10:59:58 +0100

    Added a Web API using Owin and Ninject.

---
+++ FoodManagement.Service.WebAPI/Controllers/ShoppingListController.cs
@@ -0,0 +1,73 @@
+﻿using FoodManagement.Core;
+using FoodManagement.Core.Model;
+using System;
+using System.Linq;
+using System.Web.Configuration;
+using System.Web.Http;
+
+namespace FoodManagement.Service.WebAPI.Controllers
+{
+    [RoutePrefix("api/shoppinglists")]
+    public class ShoppingListController : ApiController
+    {
+        IShoppingListService _shopService;
+        public ShoppingListController(IShoppingListService shopService)
+        {
+            _shopService = shopService;
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(_shopService.GetFamilyShoppingList(new Guid("1E81B125-F117-4040-9DBB-B92E1A034724")));
+        }
+
+        //[Authorize]
+        [Route("{familyId:Guid}")]
+        [HttpGet]
+        public IHttpActionResult Get(Guid familyId)
+        {
+            return Ok(_shopService.GetFamilyShoppingList(familyId));
+        }
+
+        [Route("{familyId:Guid}/items")]
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] ShoppingListItem item, Guid familyId)
+        {
+            //TODO: authenticate whether use has access to family shopping list
+            item.Id = Guid.NewGuid();
+            var a = _shopService.GetShoppingListItemDetailsByName(item.Item.Name);
+            if (a != null)
+                BadRequest();
+            _shopService.AddItemToFamilyShoppingList(familyId, item);
+            var returnItem = _shopService.GetShoppingListItemDetailsById(item.Id);
+            return Created(WebConfigurationManager.AppSettings["baseUrl"] + $"api/shoppinglist/{familyId}/items/{returnItem.Id}", returnItem);
+        }
+
+        [Route("{familyId:Guid}/items/{itemId:Guid}")]
+        [HttpPut]
+        public IHttpActionResult Put([FromBody] ShoppingListItem item, Guid familyId, Guid ItemId)
+        {
+            if (item.Id != null && item.Id != ItemId)
+                return BadRequest();
+
+            item.Id = ItemId;
+            if (_shopService.GetShoppingListItemDetailsById(ItemId) == null)
+                return NotFound();
+
+            _shopService.AlterShoppingListItemDetails(familyId, item);
+            return Ok(_shopService.GetShoppingListItemDetailsById(ItemId));
+        }
+
+        [Route("{familyId:Guid}/items/{itemId:Guid}")]
+        [HttpGet]
+        public IHttpActionResult Get(Guid familyId, Guid ItemId)
+        {
+            var returnItem = _shopService.GetShoppingListItemDetailsById(ItemId);
+            if (returnItem == null)
+                return NotFound();
+
+            return Ok(returnItem);
+        }
+    }
+}
---
+++ FoodManagement.Service.WebAPI/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Ninject.Web.Common.OwinHost;
+using FoodManagement.DependencyResolution;
+using Ninject;
+using Ninject.Web.WebApi.OwinHost;
+
+[assembly: OwinStartup(typeof(FoodManagement.Service.WebAPI.Startup))]
+namespace FoodManagement.Service.WebAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseNinjectMiddleware(CreateKernel).UseNinjectWebApi(config);
+        }
+
+        private static StandardKernel CreateKernel()
+        {
+            DependencyConfiguration depConfig = new DependencyConfiguration();
+            var kernel = depConfig.Kernel;
+            return kernel;
+        }
+    }
+}
